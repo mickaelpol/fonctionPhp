7ab67df051bca8ebe8ce23c411e332407ad38e93
Mickael <polmickael3@gmail.comgit config --global user.name>
2017-08-19 14:05:49 +0200

    ajout de la fonction diviser

--- fonctionAddition.php
+++ fonctionAddition.php
@@ -23,18 +23,11 @@
 			if (is_numeric($number1) && (is_numeric($number2)))
 				//je verifie d'abord que les valeurs entré dans number1 et number2 sont des valeurs numérique
 			{
-				if ($operateur === "A") {
+				if ($operateur === "A" || "a") {
 					//je verifie que l'operateur est égal a la string "A"
-					
 					$result = $number1 + $number2;
 					return $result;
 				}
-				elseif ($operateur === "a"){
-					//je verifie que l'operateur soit égale a la string "a"
-					$result = $number1 + $number2;
-					return $result;
-				}
-
 				else {
 					// si l'operateur n'es aucun des deux alors j'affiche l'erreur
 					return "tu t'es tromper d'opérateur";
@@ -43,26 +36,49 @@
 			else 
 			// si les valeurs entré ne sont pas numérique alors je renvoi l'erreur ici
 			{
-				echo "Les valeurs entré dans number1 ou number2 ne sont pas des valeurs numérique";
+				return "Les valeurs entré dans number1 ou number2 ne sont pas des valeurs numérique";
 			}
-
 		}
 
 		function Multiplication($number1, $operateur, $number2){
 			if (is_numeric($number1) && (is_numeric($number2))) {
-
-				if ($operateur === "M") {
+				if ($operateur === "M" || "m"){
 					$result = $number1 * $number2;
 					return $result;
 				}
+				else {
+					return "tu t'es tromper d'opérateur";
+				}
+			}
+			else {
+				return "Les valeurs entré dans number1 ou number2 ne sont pas des valeurs numérique";
+			}
+		}
 
-				elseif ($operateur === "m"){
-					$result = $number1 * $number2;
+		function Soustraction($number1, $operateur, $number2){
+			if (is_numeric($number1) && (is_numeric($number2))) {
+				if ($operateur === "S" || "s") {
+					$result = $number1 - $number2;
+					return $result;
+				}
+				else {
+					return "erreur d'opérateur";
+				}
+			}
+			else {
+				return "Les valeurs entré dans number1 ou number2 ne sont pas des valeurs numérique";
+			}
+		}
+
+		function Division($number1, $operateur, $number2){
+			if (is_numeric($number1) && (is_numeric($number2))) {
+				if ($operateur === "D" || "d")  {
+					$result = $number1 / $number2;
 					return $result;
 				}
 			}
 			else {
-				echo "Les valeurs entré dans number1 ou number2 ne sont pas des valeurs numérique";
+				return "Les valeurs entré dans number1 ou number2 ne sont pas des valeurs numérique";
 			}
 		}
 
@@ -70,29 +86,35 @@
 
 		$resultatAdd = Addition($number1, $operateur, $number2);
 		//stockage de la fonction addition dans une variable
-		$resultatmult = Multiplication($number1, $operateur, $number2);
+		$resultatMult = Multiplication($number1, $operateur, $number2);
+		//stockage de la fonction multiplication dans une variable
+		$resultatSous = Soustraction($number1, $operateur, $number2);
+		//stockage de la fonction multiplication dans une variable
+		$resultatDivide = Division($number1, $operateur, $number2);
 
-//--------------------------------Contenu de la phrase réponse ---------------------------------//
+
+//--------------------------------Contenu de la phrase réponse -----------------------------------//
 
 		$phraseResultatAdd = "le resultat de" . " " . $number1 . " " . "+" . " " . $number2 . " " . "=" . " " . $resultatAdd . "<br>";
 		//je stocke ma phrase resultat de l'addition dans une variable
 
-		$phraseResultatMult = "le resultat de" . " " . $number1 . " " . "x" . " " . $number2 . " " . "=" . " " . $resultatmult. "<br>";
+		$phraseResultatMult = "le resultat de" . " " . $number1 . " " . "x" . " " . $number2 . " " . "=" . " " . $resultatMult. "<br>";
+		//je stocke ma phrase resultat de la multiplication dans une variable
+
+		$phraseResultatSous = "le resultat de" . " " . $number1 . " " . "-" . " " . $number2 . " " . "=" . " " . $resultatSous. "<br>";
+		//je stocke ma phrase resultat de la soustraction dans une variable
+
+		$phraseResultatDivide = "le resultat de" . " " . $number1 . " " . "/" . " " . $number2 . " " . "=" . " " . $resultatDivide. "<br>";
+		//je stocke ma phrase resultat de la division dans une variable
 
 //---------------------------------Affichage des fonctions --------------------------------------//
-		
-		switch ($operateur){
+	
+echo $phraseResultatAdd;
+echo $phraseResultatSous;
+echo $phraseResultatMult;
+echo $phraseResultatDivide;
 
-			case "A" || "a":
-			echo $phraseResultatAdd;
-			break;
-
-			case "M" || "m":
-			echo $phraseResultatMult;
-			break;
-		}
-
-//------------------------------------------------------------------------------------------//
+//------------------------------------------------------------------------------------------------//
 		?>
 	</p>
 
