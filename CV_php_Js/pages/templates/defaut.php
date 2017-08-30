689c1c1d1da0b87865f818f0a52e3d828e3535f3
Mickael <polmickael3@gmail.comgit config --global user.name>
2017-08-30 12:03:12 +0200

    mise en place du link bootstrap et jquery

---
+++ CV_php_Js/pages/templates/defaut.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>CV Mickael Pol</title>
+	<link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.css">
+</head>
+<body>
+
+
+<h1>Bonjour c'est mon super CV</h1>
+
+
+
+
+
+<script type="text/javascript" src="./node_modules/jquery/dist/jquery.js"></script>
+</body>
+</html>
